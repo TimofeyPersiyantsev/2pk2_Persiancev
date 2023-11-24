dc3a8b7cdb35e635bc2f84fd72843223be14d3ae
timofejpersiancev79@gmail.com <timofejpersiancev79@gmail.com>
2023-11-24 18:01:27 +0500

    практика номер 11

---
+++ PZ_08/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PZ_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст");
+            string txt = Console.ReadLine();
+            Console.WriteLine("Введите подстраку");
+            string podtxt = Console.ReadLine();
+            string[] txt1 = new txt. 
+
+        }
+    }
+}
