01d2e1737113b3ad761c84861d54720ba0072cf7
timofejpersiancev79@gmail.com <timofejpersiancev79@gmail.com>
2024-05-27 17:03:51 +0500

    Практика 17

---
+++ PZ_17/Card.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_17
+{
+    internal class Card
+    {
+        // Поля класса
+        public string number;
+        public string clientFIO;
+        public DateTime validity;
+        public int secureCode;
+        public string type;
+
+        // Конструкторы класса
+        public Card(string number, string clientFIO, DateTime validity, int secureCode, string type)
+        {
+            this.number = number;
+            this.clientFIO = clientFIO;
+            this.validity = validity;
+            this.secureCode = secureCode;
+            this.type = type;
+        }
+
+        public Card(string number, string clientFIO, string type)
+        {
+            this.number = number;
+            this.clientFIO = clientFIO;
+            this.validity = DateTime.Now.AddYears(3);
+            this.secureCode = 000;
+            this.type = type;
+        }
+
+        // Метод класса
+        public void PrintInfo()
+        {
+            Console.WriteLine("Номер карты: " + number);
+            Console.WriteLine("ФИО клиента: " + clientFIO);
+            Console.WriteLine("Срок действия до: " + validity.ToShortDateString());
+            Console.WriteLine("Код безопасности: " + secureCode);
+            Console.WriteLine("Тип карты: " + type);
+        }
+    }
+}
---
+++ PZ_17/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PZ_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main()
+            {
+                Card card1 = new Card("1234567890123456", "Иванов Иван Иванович", new DateTime(2023, 12, 31), 123, "Дебетовая");
+                Card card2 = new Card("9876543210987654", "Петров Петр Петрович", "Кредитная");
+                Card card3 = new Card("1111222233334444", "Сидоров Сидор Сидорович", DateTime.Now.AddYears(2), 999, "Дебетовая");
+                Card card4 = new Card("5555666677778888", "Алексеев Алексей Алексеевич", "Кредитная");
+
+                card1.PrintInfo();
+                Console.WriteLine();
+                card2.PrintInfo();
+                Console.WriteLine();
+                card3.PrintInfo();
+                Console.WriteLine();
+                card4.PrintInfo();
+            }
+        }
+    }
+}
