0606427e286a937b3f6a0dcf9f4ae4173188df63
timofejpersiancev79@gmail.com <timofejpersiancev79@gmail.com>
2024-05-29 14:25:37 +0500

    21

---
+++ PZ_21/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PZ_21
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string buttonContent = (string)((Button)sender).Content;
+
+            if (buttonContent == "C")
+            {
+                Display.Text = "";
+            }
+            else if (buttonContent == "=")
+            {
+                try
+                {
+                    var result = new DataTable().Compute(Display.Text, null);
+                    Display.Text = result.ToString();
+                }
+                catch (Exception)
+                {
+                    Display.Text = "Error";
+                }
+            }
+            else
+            {
+                if (IsOperation(buttonContent))
+                {
+                    if (string.IsNullOrEmpty(Display.Text) || IsOperation(Display.Text[Display.Text.Length - 1].ToString()))
+                    {
+                        return;
+                    }
+                }
+                Display.Text += buttonContent;
+            }
+        }
+
+        private bool IsOperation(string input)
+        {
+            return input == "+"; _ = input == "-"; _ = input == "*" || input == "/";
+        }
+    }
+}
